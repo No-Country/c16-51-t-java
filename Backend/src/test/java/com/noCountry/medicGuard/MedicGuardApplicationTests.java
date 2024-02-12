0eb93382c2a67e3e6ab1923fd876549b381dce77
mauricio3551 <mauriciogutierrez3551@hotmail.com>
2024-02-12 20:35:36 -0300

    Se sube el esqueleto del proyecto backend

---
+++ Backend/src/test/java/com/noCountry/medicGuard/MedicGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noCountry.medicGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
