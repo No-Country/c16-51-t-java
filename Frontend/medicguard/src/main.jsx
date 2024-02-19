87caf3c8927dc81497a9c554926911fdbbe9f90b
eJos-R <72283818+TocheMansion123@users.noreply.github.com>
2024-02-19 09:26:18 -0500

    Login succesfully created

---
+++ Frontend/medicguard/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Generalogin from './Components/Login/Generalogin'
+import './Components/Login/Css/styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Generalogin></Generalogin>
+  </React.StrictMode>,
+)
