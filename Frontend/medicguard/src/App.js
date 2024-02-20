28fb3ed269e6cabfba0ef4f4dc4be9cc0357d3b2
“Ignacio <nachoatum@gmail.com>
2024-02-19 21:17:53 -0300

    Calendario finalizado al 90%

--- Frontend/medicguard/src/App.js
+++ Frontend/medicguard/src/App.js
@@ -1,51 +1,10 @@
-import './App.css';
-import { BrowserRouter, Link, Route, Router, Routes, Switch } from 'react-router-dom';
-import Calendar from './Components/Calendar';
-import Guards from './Components/Guards';
-import Home from './Components/Home';
+import Navbar from "./Components/Navbar";
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <div className='div-nav'>
-          <nav className='navbar'>
-            <ul>
-
-                <div className='list-item'>
-                  <img src='image.png' alt='Logo' className='logo' />
-                  <span> MedicGuard </span>
-                </div>
-
-              <li>
-                <Link to="/"> Home </Link>
-              </li>
-
-              <li>
-                <Link to="/calendar"> Calendario </Link>
-              </li>
-
-              <li>
-                <Link to="/guards"> Guardias </Link>
-              </li>
-
-            </ul>
-          </nav>
-
-          <hr />
-
-          <Routes>
-            <Route path="/calendar" element={<Calendar />} />
-
-            <Route path="/guards" element={<Guards />} />
-
-            <Route path="/" element={<Home />} />
-
-          </Routes>
-
-        </div>
-
-      </BrowserRouter>
+      <Navbar/>
     </>
   );
 }
---
+++ Frontend/medicguard/src/Components/Navbar.jsx
@@ -0,0 +1,54 @@
+
+import '../App.css';
+import { BrowserRouter, Link, Route , Routes  } from 'react-router-dom';
+import Calendar from '../Components/Calendar';
+import Guards from '../Components/Guards';
+import Home from '../Components/Home';
+
+function Navbar() {
+    return (
+        <>
+            <BrowserRouter>
+                <div className='div-nav'>
+                    <nav className='navbar'>
+                        <ul>
+
+                            <div className='list-item'>
+                                <img src='image.png' alt='Logo' className='logo' />
+                                <span> MedicGuard </span>
+                            </div>
+
+                            <li>
+                                <Link to="/"> Inicio </Link>
+                            </li>
+
+                            <li>
+                                <Link to="/calendar"> Calendario </Link>
+                            </li>
+
+                            <li>
+                                <Link to="/guards"> Guardias </Link>
+                            </li>
+
+                        </ul>
+                    </nav>
+
+                    <hr />
+
+                    <Routes>
+                        <Route path="/calendar" element={<Calendar />} />
+
+                        <Route path="/guards" element={<Guards />} />
+
+                        <Route path="/" element={<Home />} />
+
+                    </Routes>
+
+                </div>
+
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default Navbar;
