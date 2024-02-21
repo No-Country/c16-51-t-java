cdb80e0bd60ea5306d3b56c93359968161e8e10f
majuarezmagarinios <majuarezmagarinios@gmail.com>
2024-02-21 18:24:09 -0300

    JWT se implementó parcialmente, devuelve 200 de Postman al iniciar sesión y registrarse

---
+++ Backend/src/main/java/com/noCountry/medicGuard/security/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.noCountry.medicGuard.security.config;
+
+import com.noCountry.medicGuard.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        (authz) ->
+                                authz
+                                        //                  Path to log in before the user was authenticated
+                                        .requestMatchers("/api/auth/**")
+                                        .permitAll()
+
+                                        //                  Path to access admin
+                                        .requestMatchers("/api/admin/**")
+                                        .hasRole("ADMIN")
+
+                                        //                  Path to access user
+                                        .requestMatchers("/api/user/**")
+                                        .hasRole("USER")
+                                        .anyRequest()
+                                        .authenticated())
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(
+                        sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(Customizer.withDefaults())
+                .exceptionHandling(
+                        httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(
+                                authenticationEntryPoint))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
+
